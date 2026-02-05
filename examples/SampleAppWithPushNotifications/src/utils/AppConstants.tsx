4c83a0825e4d330b6eaafb4fedbad6c2abde0b84
harshmohan-rgb <harsh.mohan@cometchat.com>
2026-02-05 12:32:33 +0530

    v5.2.10

--- examples/SampleAppWithPushNotifications/src/utils/AppConstants.tsx
+++ examples/SampleAppWithPushNotifications/src/utils/AppConstants.tsx
@@ -5,7 +5,7 @@ export const AppConstants = {
   appId: '26580020f03ff346',
   region: 'in',
   subscriptionType: 'ALL_USERS',
-  versionNumber: 'V5.2.9',
+  versionNumber: 'V5.2.10',
   webClientId:
     '',
   iosClientId:
